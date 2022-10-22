08c87dcf7e472da1926a7ef33988146c5364b92c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-22 22:21:08 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user"
+class Student < User
 
+attr_writer :knowledge
+def initialize
+    @knowledge = []
+end
+def learn(new_knowledge)
+    @knowledge << new_knowledge
+
+end
+def knowledge
+    @knowledge
+end
 end
